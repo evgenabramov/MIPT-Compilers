9ab834a6e8c0b951d28de624317b3fe4af2f91dc
Evgeny Abramov <abramov.ed@phystech.edu>
2020-08-19 18:17:52 +0000

    Update README

--- docker/compilers-image/install_deps.sh
+++ docker/compilers-image/install_deps.sh
@@ -5,26 +5,26 @@ set -e -x
 apt-get update
 
 apt-get install -y \
-	ssh \
-	make \
-	cmake \
-	ninja-build \
-	git \
-	clang-8 \
-	clang-format-8 \
-	clang-tidy-8 \
-	python3 \
-	python3-pip \
-	python3-venv \
-	ca-certificates \
-	openssh-server \
-	rsync \
-	lldb-8 \
-	vim \
-	gdb \
-	libboost-all-dev \
-	wget \
-	autoconf \
+    ssh \
+    make \
+    cmake \
+    ninja-build \
+    git \
+    clang-8 \
+    clang-format-8 \
+    clang-tidy-8 \
+    python3 \
+    python3-pip \
+    python3-venv \
+    ca-certificates \
+    openssh-server \
+    rsync \
+    lldb-8 \
+    vim \
+    gdb \
+    libboost-all-dev \
+    wget \
+    autoconf \
     tree \
     bison \
     flex \
@@ -35,9 +35,9 @@ apt-get install -y \
     qemu-user-static
 
 pip3 install \
-	click \
-	gitpython \
-	python-gitlab \
-	termcolor \
-	virtualenv
+    click \
+    gitpython \
+    python-gitlab \
+    termcolor \
+    virtualenv
 
